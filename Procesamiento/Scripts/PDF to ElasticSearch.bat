cac0a8bce81d98ca30ece4543b7fd27e5ba36ad3
Felipe Fernandez <felipefelizx@gmail.com>
2023-05-28 02:40:56 -0400

    Feature - Bat for 3 script

---
+++ Procesamiento/Scripts/PDF to ElasticSearch.bat
@@ -0,0 +1,24 @@
+@echo off
+
+echo Ejecutando PDF to txt in folder.py
+python "PDF to txt in folder.py"
+if %errorlevel% neq 0 (
+    echo Error al ejecutar PDF to txt in folder.py
+    exit /b
+)
+
+echo Ejecutando Preprocesamiento.py
+python "Preprocesamiento.py"
+if %errorlevel% neq 0 (
+    echo Error al ejecutar Preprocesamiento.py
+    exit /b
+)
+
+echo Ejecutando procesado con pdf to Elastisearch.py
+python "procesado con pdf to Elastisearch.py"
+if %errorlevel% neq 0 (
+    echo Error al ejecutar procesado con pdf to Elastisearch.py
+    exit /b
+)
+
+echo Todos los scripts se han ejecutado correctamente.
