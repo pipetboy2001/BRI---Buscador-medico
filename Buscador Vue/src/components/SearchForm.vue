e6c4246632e4e928c1584d4bd003b83a2c4a981b
Felipe Fernandez <felipefelizx@gmail.com>
2023-06-01 20:05:20 -0400

    Feature - Bootstrap with Accordion

--- Buscador Vue/src/components/SearchForm.vue
+++ Buscador Vue/src/components/SearchForm.vue
@@ -2,12 +2,22 @@
   <div>
     <input type="text" v-model="query" placeholder="Ingresa tu consulta de búsqueda" />
     <button @click="search">Buscar</button>
-
     <div v-if="results.length === 0 && searchCompleted">No se encontraron resultados.</div>
     <div v-else>
       <div v-for="result in results" :key="result._id">
-        <h3>{{ result._source.nombre }}</h3>
-        <p>{{ result._source.contenido }}</p>
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button class="accordion-button" type="button" @click="toggleAccordion(result._id)">
+              <h3>{{ result._source.nombre }}</h3>
+            </button>
+          </h2>
+          <div :id="'collapse-' + result._id" class="accordion-collapse collapse"
+            :class="{ 'show': activeId === result._id }">
+            <div class="accordion-body">
+              <p>{{ result._source.contenido }}</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -20,11 +30,20 @@ export default {
   data() {
     return {
       query: '',
-      results: [],
-      searchCompleted: false,
+      results: [],  // Los resultados de la búsqueda
+      searchCompleted: false,  // Indica si la búsqueda se ha completado
+      activeId: null  // ID del resultado activo en el acordeón
     };
   },
+
   methods: {
+    toggleAccordion(id) {
+      if (this.activeId === id) {
+        this.activeId = null;  // Si se hace clic en el resultado activo, se cierra el acordeón
+      } else {
+        this.activeId = id;  // Se establece el nuevo resultado activo
+      }
+    },
     search() {
       this.results = [];
       this.searchCompleted = false;
@@ -47,3 +66,5 @@ export default {
   },
 };
 </script>
+
+
